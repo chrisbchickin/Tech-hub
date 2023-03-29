4392992ee8e5b1690da2cf997042283929a2b211
Chrisbchicken <chrsbnjmn@gmail.com>
2023-03-29 18:42:02 -0500

    fix login route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -35,9 +35,6 @@ router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
 
-    const dbPassword = userData.dataValues.password;
-    const reqPassword = req.body.password;
-
     if (!userData) {
       res
         .status(400)
@@ -45,26 +42,25 @@ router.post('/login', async (req, res) => {
       return;
     }
 
-    const validPassword = await checkPassword(dbPassword, reqPassword);
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
       
-      
-      if (!validPassword) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email or password, please try again' });
-        return;
-      }
-      
-    // req.session.save(() => {
-    //   req.session.user_id = userData.id;
-    //   req.session.logged_in = true;
-    //   res.json(userData);
-    // });
-      console.log(userData);
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
   } catch (err) {
     res.status(400).json(err);
     console.log(err);
-    console.log('there was an error');
   }
 });
 // logout route for user
