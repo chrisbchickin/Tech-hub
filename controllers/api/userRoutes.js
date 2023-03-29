785b7670aeb1efbaba3c5b5a50faec60692d5d46
Chrisbchicken <chrsbnjmn@gmail.com>
2023-03-29 18:02:26 -0500

    fix signup route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -23,10 +23,10 @@ router.post('/', async (req, res) => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
 
-      res.status(200).json(userData, { test:'test' });
+      res.status(200).json(userData);
     });
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
     console.log(err);
   }
 });
