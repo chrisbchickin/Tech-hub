e188d1cde40c44ef1f7b4733f4379b22aca25716
Chrisbchicken <chrsbnjmn@gmail.com>
2023-03-29 18:15:05 -0500

    fix login route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -54,11 +54,11 @@ router.post('/login', async (req, res) => {
           .json({ message: 'Incorrect email or password, please try again' });
         return;
       }
-
+      res.json(userData);
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-      res.json(userData);
+      
     });
 
   } catch (err) {
