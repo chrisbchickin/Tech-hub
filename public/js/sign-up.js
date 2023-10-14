c455a9ed5e999cb2f3f5964e6a92b6ef4bc69021
Chrisbchicken <chrsbnjmn@gmail.com>
2023-10-14 14:59:41 -0500

    Pushing to heroku

--- public/js/sign-up.js
+++ public/js/sign-up.js
@@ -9,15 +9,16 @@ const signupFormHandler = async (event) => {
     if (username && email && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ username, email, password   }),
+        body: JSON.stringify({ username, email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
       if (response.ok) {
         document.location.replace('/');
       } else {
-        alert(response.statusText);
-        console.log(response.body);
+        // alert(response.statusText);
+        const errorData = await response.json();
+        alert(errorData.message);
       }
     }
   };
